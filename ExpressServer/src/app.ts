e0397baf670ad8e4db3c893cba2075008f7cc2bd
Nabeegh-Ahmed <nabeegh08@gmail.com>
2022-02-02 13:22:04 +0500

    Auth added

--- ExpressServer/src/app.ts
+++ ExpressServer/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { PrismaClient } from '@prisma/client'
+
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -8,32 +8,10 @@ const app = express()
 app.use(express.json())
 app.use(cors());
 
-const prisma = new PrismaClient()
+import userRoutes from "./routes/userRoutes"
 
-const querySample = async() => {
-    const samples = await prisma.sample.findMany()
-    console.log(samples)
-}
+app.use('/api/user', userRoutes)
 
-const createSample = async() => {
-    await prisma.sample.create({
-        data: {
-            name: 'sample1'
-        }
-    })
-    const samples = await prisma.sample.findMany()
-    console.log(samples)
-}
-
-const updateSample = async() => {
-    const sample = await prisma.sample.update({
-        where: {id: 1},
-        data: {name: "SampleUpdated"}
-    })
-    console.log(sample)
-}
-
-createSample().catch(err => console.log(err)).finally(() => prisma.$disconnect())
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
